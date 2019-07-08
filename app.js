34194fdd89baafcee640a54eb676ee49a903beb6
charles mancuso <charlesmancuso66@yahoo.com>
2019-07-07 17:47:06 -0700

    added users and tweets route, bodyParser

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -12,3 +15,7 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("hi"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
